d7f1f864cc9832ca81d19b4d25b3df2b26cc9f67
wadevanorden <wadevanorden@gmail.com>
2023-03-28 10:53:24 -0400

    changes

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ def overview():
     return "hello world"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
 
 
 '''''''''
--- src/main.wsgi
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.insert(0, '/var/www/html/cosmos/')
-
-from main import app as application
