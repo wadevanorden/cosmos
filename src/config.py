be183771d66a4b0dce884030409d07e401b2865d
wadevanorden <wadevanorden@gmail.com>
2023-03-29 13:57:48 -0400

    name change

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import os
 
 SECRET_KEY = os.getenv('secret-key')
-MYSQL_HOST = 'aurora-mysql-achievements-dev-instance-1.cku0taepgyyj.us-east-1.rds.amazonaws.com'
+MYSQL_HOST = 'aurora-mysql-cosmos-dev.cluster-cku0taepgyyj.us-east-1.rds.amazonaws.com'
 MYSQL_USER = 'admin'
 MYSQL_PASSWORD = os.getenv('mysql-dev-password')
 MYSQL_DB = 'achievements_main_dev'
