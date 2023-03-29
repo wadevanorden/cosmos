0bf5501ef81109848de6c03424e0d279f88ad8bd
wadevanorden <wadevanorden@gmail.com>
2023-03-28 21:14:44 -0400

    fix

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def overview():
-    return render_template('login.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
