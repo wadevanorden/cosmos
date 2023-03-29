985dfbf0ec5f13ad1822247f4664886e4d7bc939
wadevanorden <wadevanorden@gmail.com>
2023-03-29 14:18:13 -0400

    fix

--- src/config.py
+++ src/config.py
@@ -4,4 +4,4 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 MYSQL_HOST = 'aurora-mysql-cosmos-dev.cluster-cku0taepgyyj.us-east-1.rds.amazonaws.com'
 MYSQL_USER = 'admin'
 MYSQL_PASSWORD = os.getenv('MYSQL_DEV_PASSWORD')
-MYSQL_DB = 'achievements_main_dev'
+MYSQL_DB = 'cosmos_dev'
