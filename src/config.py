d88be925f81afeaf8abf4a483f9fda53050b864a
wadevanorden <wadevanorden@gmail.com>
2023-03-29 13:35:22 -0400

    change

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 import os
 
-SECRET_KEY = 'GDtfDCFYjD'
+SECRET_KEY = os.getenv('secret-key')
 MYSQL_HOST = 'aurora-mysql-achievements-dev-instance-1.cku0taepgyyj.us-east-1.rds.amazonaws.com'
 MYSQL_USER = 'admin'
 MYSQL_PASSWORD = os.getenv('mysql-dev-password')
