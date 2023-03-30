6b05ff42f6d58187df2751d582752199b438aab6
wadevanorden <wadevanorden@gmail.com>
2023-03-29 23:36:44 -0400

    test

--- src/main.py
+++ src/main.py
@@ -131,8 +131,7 @@ def steam_authorize():
                     input_apps.append(connection_mapping['app_id'])
                 else:
                     new_app_id = random.getrandbits(64)
-                    cursor.execute('INSERT INTO Connections_Mapping (app_id,appid) VALUES (%s, %s)', 
-                           [new_app_id, app['appid']])
+                    cursor.execute('INSERT INTO Connections_Mapping (app_id,appid) VALUES (%s, %s)', [new_app_id, app['appid']])
                     mysql.connection.commit()
                     input_apps.append(new_app_id)
             for app_id in input_apps:
